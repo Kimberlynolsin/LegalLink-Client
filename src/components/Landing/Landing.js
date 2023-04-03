0b645e5f811abd0b1a20978a1125fa21ae2d3bd2
Kimberlynolsin <kimberly.nolsin@gmail.com>
2023-04-03 17:12:23 -0400

    added border to homepage

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import "react-sweet-progress/lib/style.css";
 import info from "../../assets/icons/help.png";
+import "react-sweet-progress/lib/style.css";
+import { Progress } from "react-sweet-progress";
 
 const Landing = ({ status }) => {
   const [openDialog, setOpenDialog] = useState(false);
@@ -14,17 +16,27 @@ const Landing = ({ status }) => {
     status.map((element) => {
       return (
         <>
-          <div key={element.id}>
+          <div key={element.id} className="landing__backdrop">
             <div className="landing__container">
               <div className="landing__progress-work" key={element.id}>
                 <h3 className="landing__progress-work__title">
                   {element.title}
                 </h3>
-                <p className="landing__status">Status: {element.status}</p>
               </div>
-              <div className="landing__solution">
-                <p className="landing__resolution">{element.resolution}</p>
-              </div>
+            </div>
+            <div className="landing__solution">
+              <p className="landing__status">Status: {element.status}</p>
+              <p className="landing__resolution">{element.resolution}</p>
+            </div>
+            <div className="landing__test">
+              <Progress
+                theme={{
+                  active: {
+                    color: "#afaab9",
+                  },
+                }}
+                percent={element.percentage}
+              />
             </div>
             <div>
               <hr></hr>
@@ -38,30 +50,30 @@ const Landing = ({ status }) => {
     <section className="landing">
       <h2 className="landing__title">HOME</h2>
       <div className="landing__flexed">
-      <div className="landing__content">{cards}</div>
-      <div className="landing__estimate">
-        <div className="landing__estimate__box">
-          <h3 className="landing__estimate__title">
-            ESTIMATED COURT CASE DATE:
-          </h3>
-          <div className="landing__estimate__img">
-            <img
-              className="landing__estimate__info"
-              src={info}
-              onClick={toggleModal}
-              alt="help"
-            ></img>
+        <div className="landing__content">{cards}</div>
+        <div className="landing__estimate">
+          <div className="landing__estimate__box">
+            <h3 className="landing__estimate__title">
+              ESTIMATED COURT CASE DATE:
+            </h3>
+            <div className="landing__estimate__img">
+              <img
+                className="landing__estimate__info"
+                src={info}
+                onClick={toggleModal}
+                alt="help"
+              ></img>
+            </div>
           </div>
+          <p className="landing__estimate__details">To be determined</p>
+          {openDialog && (
+            <p className="landing__estimate__popup">
+              Please be advised that you will receive an official notification
+              in your mailbox regarding the scheduled date of your court
+              hearing, prior to the actual proceedings.
+            </p>
+          )}
         </div>
-        <p className="landing__estimate__details">To be determined</p>
-        {openDialog && (
-          <p className="landing__estimate__popup">
-            Please be advised that you will receive an official notification in
-            your mailbox regarding the scheduled date of your court hearing,
-            prior to the actual proceedings.
-          </p>
-        )}
-      </div>
       </div>
     </section>
   );
